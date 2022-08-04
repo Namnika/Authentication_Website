36ebcd87f90c345ae6c28b41e09fd7d7322485f5
Namnika <janbandhunamnika@gmail.com>
2022-08-04 17:05:45 +0530

    Update all files

--- app.js
+++ app.js
@@ -246,6 +246,10 @@ app.get("/logout", function(req, res){
 });
 
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port, function(){
   console.log("Server has started on portal 3000!");
 });
