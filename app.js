ea7fd80da983f575627c9db5c50f5c09cb334aa0
Namnika <janbandhunamnika@gmail.com>
2022-08-04 18:52:10 +0530

    update files

--- app.js
+++ app.js
@@ -250,6 +250,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port, function(){
-  console.log("Server has started on portal 3000!");
+app.listen(port, function() {
+  console.log("Server has started successfully!");
 });
