224c0957694dcd7f655349f7ff4a2dc58322f580
Namnika <janbandhunamnika@gmail.com>
2022-08-05 09:34:31 +0530

    Update all files

--- app.js
+++ app.js
@@ -67,12 +67,10 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-    proxy: true
   },
   function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id },
-      function (err, user) {
+    User.findOrCreate({ googleId: profile.id, username: profile.emails[0].value },
+      (err, user) {
       return cb(err, user);
     });
   }
